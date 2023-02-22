a05ab0b6728ac40309f6154870d30e69e972a25b
BrianMere <brianmere@cox.net>
2023-02-22 13:01:14 -0800

    Messing with Simulation Probes

--- HW6_Control_Unit/HW6_Control_Unit.srcs/sim_1/new/sim.sv
+++ HW6_Control_Unit/HW6_Control_Unit.srcs/sim_1/new/sim.sv
@@ -8,34 +8,61 @@ module sim(
     );
 
     logic reset, clk;
+//    logic intr, IOBUS_wr;
+//    logic [31:0] IOBUS_in, IOBUS_out, IOBUS_addr;
 
     Otter_MCU MCU(
         .RST(reset),
-        .CLK(clk),
-        // TODO:
+        .CLK(clk)
+//        .INTR(intr),
+//        .IOBUS_IN(IOBUS_in),
+//        .IOBUS_OUT(IOBUS_out),
+//        .IOBUS_ADDR(IOBUS_addr),
+//        .IOBUS_WR(IOBUS_wr)
     );
 
     initial begin
         reset = 1;
-        #1 reset = 0;
-        #1
-        #1        
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
-        #1
+        clk = 0;
+        //intr = 0; // can keep low
+        #1 switch_clk();
+        #1 switch_clk();
+        reset = 0;
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        #1 switch_clk();
+        
+        
     end
+    
+    function void switch_clk();
+        clk = ~clk;
+    endfunction
 
 endmodule
